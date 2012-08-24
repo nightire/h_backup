8236ffd8f2dace75894e78597e6d55be0eba710b
Albert Yu <dev.yufan@gmail.com>
2012-08-24 12:55:56 +0800

    Initial commit - create the application - modify the Gemfile - set up with postgresql database environment - ready to go to deploy on the heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+H::Application.config.secret_token = '1368af2a7db514ee103ec74261c14a581bb917ee72bec105d72481748c1ad362c360517a8411acd7b89b59768645af8d334aaa2958e36bcfa61b33b2595731fb'

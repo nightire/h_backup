b1d23d3a32ce1a96a99fc48ec5219d8d226fa8ed
Albert Yu <dev.yufan@gmail.com>
2012-08-25 16:30:17 +0800

    小小的语法改善

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120825015703) do
+ActiveRecord::Schema.define(:version => 20120825081710) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(:version => 20120825015703) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", %w(useremail), :name => "index_users_on_useremail", :unique => true
 
 end

ebb9aa6817a128bcee10cffd7e16e12d9d43ff8e
Albert Yu <dev.yufan@gmail.com>
2012-08-26 09:15:09 +0800

    实现注册表单的基本功能，允许用户注册并导航至用户信息页面

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,4 +16,28 @@ describe "User Pages" do
     it { should have_selector 'title',  text: user.username }
     it { should have_selector 'h1',     text: user.username }
   end
+
+  describe "Signup form page" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Account Name", with: "Albert"
+        fill_in "E-mail",       with: "albert@yucd.me"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
